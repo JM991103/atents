c54dc5e03a32d72010078ac5255209ffcc4d7092
JM991103 <ljn1103@naver.com>
2022-08-23 12:03:03 +0900

    완성

--- 01test/01test/Orc.cs
+++ 01test/01test/Orc.cs
@@ -10,11 +10,18 @@ namespace _01test
 {
     public class Orc : Character
     {
-        public Orc(string newName) : base(newName)  //생성자
+        /// <summary>
+        /// 이름을 입력받는 생성자(생성자는 상속이 안되기 때문에 항상 새로 만들어 주어야 한다)
+        /// </summary>
+        /// <param name="newName">새 이름</param>
+        public Orc(string newName) : base(newName) //Character(string newName) 실행됨
         {
             //Orc의 이름 출력
         }
 
+        /// <summary>
+        /// 스테이터스 생성
+        /// </summary>
         public override void GenerateStatus()
         {
             base.GenerateStatus();
