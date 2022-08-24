3f1d288f32c4a2907f6c80513c424293c415ccc2
JM991103 <ljn1103@naver.com>
2022-08-24 15:08:45 +0900

    유니티실습_4

--- 02_Shooting/Assets/Script/Move.cs
+++ 02_Shooting/Assets/Script/Move.cs
@@ -25,6 +25,7 @@ public class Move : MonoBehaviour
     /// </summary>
     private void Update()
     {
+
         //    //Vector3 : 벡터를 표현하기 위한 구조체. 위치를 표현할 때도 많이 사용한다.
         //    //벡터 : 힘의 방향과 크기를 나타내는 단위 
 
@@ -35,7 +36,7 @@ public class Move : MonoBehaviour
         //    //new Vector3(0, 1, 0);   // 위쪽    Vector3.up;
         //    //new Vector3(0, -1, 0);  // 아래쪽  Vector3.down;
 
-           transform.position += (speed * Time.deltaTime * dir);   //아래쪽 방향으로 speed 만큼 움직여라(매 프레임마다)
+           transform.position += (speed * Time.deltaTime * dir);   //아래쪽 방향으로 speed 만큼 움직여라 (매 프레임마다)
 
         //    //Time.deltaTime : 이전 프레임에서 지금 프레임까지 걸린 시간
         //    //transform.position += (Vector3.down * speed * Time.deltaTime); //아래쪽 방향으로 speed만큼 움직여라 (매 초마다)
@@ -45,23 +46,28 @@ public class Move : MonoBehaviour
     //    //Test_OidInputmanager();
     public void MoveInput(InputAction.CallbackContext context)
     {
-        if (context.started)    // 매핑된 키가 누른 직후
-        {
-            Debug.Log("입력들어옴 - started");
-            dir = Vector3.zero;
-        }
-        if (context.performed)  // 매핑된 키가 확실하게 눌려졌다.
-        {
-            Debug.Log("입력들어옴 - performed");
-            Vector2 InputDir = context.ReadValue<Vector2>();
-            dir = InputDir;
-            Debug.Log(InputDir);
-        }
-        if (context.canceled)   // 매핑된 키가 떨어졌을 때
-        {
-            Debug.Log("입력들어옴 - canceled");
-            dir = Vector3.zero;
-        }
+        //if (context.started)    // 매핑된 키가 누른 직후
+        //{
+        //    Debug.Log("입력들어옴 - started");
+        //    dir = Vector3.zero;
+        //}
+        //if (context.performed)  // 매핑된 키가 확실하게 눌려졌다.
+        //{
+        //    Debug.Log("입력들어옴 - performed");
+           
+        //}
+        //if (context.canceled)   // 매핑된 키가 떨어졌을 때
+        //{
+        //    Debug.Log("입력들어옴 - canceled");
+        //    dir = Vector3.zero;
+
+        //    //vector : 방향과 크기
+        //    //vector2 : 유니티에서 제공하는 구조체(struct). 2차원 벡터를 표현하기 위한 구조체(x, y)
+        //    //Vector3 : 유니티에서 제공하는 구조체(struct). 3차원 벡터를 표현하기 위한 구조체(x, y, z)
+        //}
+        Vector2 InputDir = context.ReadValue<Vector2>();    // 어느 방향으로 움직여야 하는지를 입력받음
+        Debug.Log(InputDir);    //InputDir의 위치를 나타냄
+        dir = InputDir;     //dir.x = InputDir.x; dir.y = InputDir.y; dir.z = 0.0f
     }
 
     public void fire(InputAction.CallbackContext context)
